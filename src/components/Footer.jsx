a60236174dea3bbd904673553df675b4ec550e4a
Youssef Rabei <youssefrabei2003@gmail.com>
2020-09-23 19:10:44 +0200

    🚚 Change the Social Path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Social } from "./";
+import { Social } from "./Social";
 import { FaCode, FaRegHeart, FaCoffee } from "react-icons/fa";
 
 import "./footer.css";
